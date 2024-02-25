f0ad0822c8484c300cde6c4deaa7089cf4ba9155
aleifericsson <alifnafili@gmail.com>
2024-02-25 22:38:22 +0000

    huge progress

--- src/components/buttonOverlay.js
+++ src/components/buttonOverlay.js
@@ -1,12 +1,12 @@
 import {render, remove, create, addClass, remClass, find, write, detect, undetect, style, hasClass} from "../scripts/QoL"
 import buttons from "../images/Buttons_updated.png"
 import { togglePrompt } from "./prompts";
-import { moveTowards } from "../scripts/spritecanvas";
+import { drawSC, moveTowards, setShow, teleport } from "../scripts/spritecanvas";
 
 const butSize = 32;
 let butOv;
 let pointer = false;
-let interval_list = [];
+let pointTime = [];
 
 const buttonOverlay = (width, height) =>{
     butOv = create("div");
@@ -56,7 +56,8 @@ const toggleShake = (e) => {
 
 const togglePointer = (evt) => {
     const but = find("#pointer")
-    const rect = document.body.getBoundingClientRect();
+    const canv = find(".layer-0")
+    const rect = canv.getBoundingClientRect();
     let mousePos = {x:0,y:0};
 
     const updatePointer = (evt) =>{
@@ -66,23 +67,28 @@ const togglePointer = (evt) => {
             y: evt.clientY - rect.top
         };
     }
+    updatePointer(evt);
 
     if (pointer){
         pointer = false;
         remClass(but, ["selected"]);
-        clearInterval(interval_list[0]);
-        interval_list = [];
+        clearInterval(pointTime[0]);
+        pointTime = [];
         undetect(document,"mousemove", updatePointer);
+        setShow(1, false);
     }
     else{
         pointer = true;
         addClass(but, ["selected"])
         detect(document.body,"mousemove", updatePointer);
 
-        interval_list.push(setInterval(() => {
+        pointTime.push(setInterval(() => {
             
             moveTowards(0, mousePos.x,mousePos.y);
+            drawSC(1,"increment","none");
+            teleport(1,Math.floor(mousePos.x/64)*64, Math.floor(mousePos.y/64)*64);
         }, 50));
+        setShow(1, true);
     }
 }
 
--- src/scripts/spritecanvas.js
+++ src/scripts/spritecanvas.js
@@ -1,42 +1,61 @@
 import {render, remove, create, addClass, hasClass, remClass, find, write, detect, undetect, style, attribs} from "./QoL"
 import carsrc from "../images/Car_updated.png"
 import { getTiles } from "./canvasFuncs"
+import hlsrc from "../images/decor.png"
 
 let sc_list = []
 
-const spriteCanvas = (wrapper, name, size, imgsrc, x, y, speed) =>{
+const spriteCanvas = (wrapper, name, size, imgsrc, x, y, speed, show, frames) =>{
 
-    const carele = create("canvas");
-    addClass(carele, ["spritecanvas"]);
-    attribs(carele, ["id", "width", "height"], ["car", `${size}px`, `${size}px`]);
+    const canv = create("canvas");
+    addClass(canv, ["spritecanvas"]);
+    attribs(canv, ["id", "width", "height"], [name, `${size}px`, `${size}px`]);
 
     const img = new Image();
     img.src = imgsrc;
-    const ctx = carele.getContext("2d");
-    img.onload = function() {
-        ctx.clearRect(0,0,size,size);
-        ctx.drawImage(img, 0, 0, size, size, 0, 0, size,size);
-    }
 
-    style(carele, `
+    style(canv, `
         position:absolute;
+        pointer-events:none;
         top: ${y}px;
         left: ${x}px;
     `);    
 
-    sc_list.push({ 
+    let obj = { 
         name,
-        ele: carele, 
+        size,
+        ele: canv, 
         x,
         y,
         direction: "left",
         img,
-        speed
-    });
+        speed,
+        direction_data: {"none":0},
+        draw_index: 0,
+        frame:0,
+        show,
+        frames,
+        updates_per_frames: 2,
+        timer: 1,
+    };
 
-    render(wrapper, carele);
+    if (name === "car"){
+        obj.direction_data = {"left":0,"downleft":1,"down":2,"upright":3,"downright":4,"upleft":5,"up":6,"right":7}
+    }
 
-    return carele;
+    sc_list.push(obj);
+
+
+    if (show){
+        const ctx = canv.getContext("2d");
+        img.onload = function() {
+            ctx.clearRect(0,0,size,size);
+            ctx.drawImage(img, 0, 64*0, size, size, 0, 0, size,size);
+        }
+        render(wrapper, canv);
+    }
+
+    return canv;
 }
 
 const moveTowards = (index, x, y) => {
@@ -51,11 +70,9 @@ const moveTowards = (index, x, y) => {
     const ny = obj.y+uy;
     sc_list[index].x = nx;
     sc_list[index].y = ny;
-    style(ele, `
-        position:absolute;
-        top: ${ny}px;
-        left: ${nx}px;
-    `);
+    const size = sc_list[index].size
+    if (mag>obj.speed){
+    teleport(index, nx-size/2, ny-size/2)
     let angle = Math.atan(-uy/ux);
     if(ux < 0){
         if (-uy < 0){
@@ -76,11 +93,63 @@ const moveTowards = (index, x, y) => {
     else if (angle <= -112.5 && angle >= -157.5) direction = "downleft"
     else if (angle >= 157.5 && angle <= -157.5) direction = "left"
 
+    drawSC(0, "increment", direction);
+    }
+}
+
+const setShow = (index, show) => {
+    const wrapper = find(".wrapper");
+    sc_list[index].show = show;
+    if(show) {
+        if (find(`#${sc_list[index].name}`) === null) render(wrapper, sc_list[index].ele);
+        if (index === 1) drawSC(1,1,"none")
+    }
+    else {
+        if (find(`#${sc_list[index].name}`) !== null) remove(wrapper, sc_list[index].ele);
+    }
+}
+
+const drawSC = (index, frame, direction) => {
+    let fram = frame;
+    if (frame === "increment"){
+        if (sc_list[index].timer === sc_list[index].updates_per_frames){
+            fram = sc_list[index].frame + 1;
+            sc_list[index].frame = fram;
+            sc_list[index].timer = 1;
+        }
+        else {
+            sc_list[index].timer += 1;
+        }
+    }
+    if (fram === sc_list[index].frames){
+        if (sc_list[index].name === "highlight") fram = 1;
+        else fram = 0
+        sc_list[index].frame = fram;
+    }
+    if (sc_list[index].timer === 1){
     sc_list[index].direction = direction;
+    const draind = sc_list[index].direction_data[direction];
+    sc_list[index].draw_index = draind;
+    const ctx = sc_list[index].ele.getContext("2d");
+    const size = sc_list[index].size;
+    const img = sc_list[index].img;
+    ctx.clearRect(0,0,size,size);
+    ctx.drawImage(img, size*fram, size*draind, size, size, 0, 0, size,size);
+    }
+}
+
+const teleport = (index, x, y) =>{
+    style(sc_list[index].ele, `
+        position:absolute;
+        pointer-events:none;
+        top: ${y}px;
+        left: ${x}px;
+    `);
 }
 
 const initSC = (wrapper) =>{
-    const sc = spriteCanvas(wrapper, "car", 64, carsrc, 300, 200, 5);
+    const car = spriteCanvas(wrapper, "car", 64, carsrc, 300, 200, 5, true,3);
+    const highlight = spriteCanvas(wrapper, "highlight", 64, hlsrc, 0,0, 0, false,8)
 }
 
-export{initSC, moveTowards}
+export{initSC, moveTowards ,setShow, drawSC, teleport}
